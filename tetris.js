4e85b1716ae1962210954f61211b18297d496858
太田佳希 <suka6411@gmail.com>
2019-10-22 09:48:58 +0900

    add: tetrimino

--- tetris.js
+++ tetris.js
@@ -1,4 +1,7 @@
 let tetris = {
+    x: 4,
+    x: 4,
+    y: 1,
     board: [
         [2, 2, 2, 0, 0, 0, 0, 0, 0, 2, 2, 2],
         [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
@@ -25,20 +28,71 @@ let tetris = {
     ],
 };
 
-function drawScreen() {
-    let canvas = document.getElementById('tetris');
-    let context = canvas.getContext('2d');
+let tetrimino = {
+    I: [
+        [1, 1, 1, 1],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+    ],
+    O: [
+        [1, 1],
+        [1, 1],
+    ],
+    S: [
+        [0, 1, 1],
+        [1, 1, 0],
+        [0, 0, 0],
+    ],
+    Z: [
+        [1, 1, 0],
+        [0, 1, 1],
+        [0, 0, 0],
+    ],
+    J: [
+        [1, 1, 1],
+        [0, 0, 1],
+        [0, 0, 0],
+    ],
+    L: [
+        [1, 1, 1],
+        [1, 0, 0],
+        [0, 0, 0],
+    ],
+    T: [
+        [1, 1, 1],
+        [0, 1, 0],
+        [0, 0, 0],
+    ],
+};
 
-    context.fillStyle = "gray";
-    context.strokeStyle = "black";
-    context.lineWidth = 2;
+function drawTetrimino(tetrimino) {
+    for (let [y, row] of tetrimino.entries()) {
+        for (let [x, cell] of row.entries()) {
+            if (cell == 1) {
+                tetris.context.beginPath();
+                tetris.context.fillStyle = "lightblue";
+                tetris.context.rect((tetris.x + x) * 20, (tetris.y + y) * 20, 20, 20);
+                tetris.context.fill();
+                tetris.context.stroke();
+            }
+        }
+    }
+}
+
+function drawScreen() {
     for (let [y, row] of tetris.board.entries()) {
         for (let [x, cell] of row.entries()) {
+            if (x == tetris.x && y == tetris.y) {
+                drawTetrimino(tetris.tetrimino);
+            }
             switch (cell) {
                 case 2:
-                    context.rect(x * 20, y * 20, 20, 20);
-                    context.fill();
-                    context.stroke();
+                    tetris.context.beginPath();
+                    tetris.context.fillStyle = "gray";
+                    tetris.context.rect(x * 20, y * 20, 20, 20);
+                    tetris.context.fill();
+                    tetris.context.stroke();
                     break;
             }
         }
@@ -46,6 +100,11 @@ function drawScreen() {
 }
 
 function initTetris() {
+    let canvas = document.getElementById('tetris');
+    let context = canvas.getContext('2d');
+    context.lineWidth = 2;
+    tetris.context = context;
+    tetris.tetrimino = tetrimino.I;
     drawScreen();
 }
 
